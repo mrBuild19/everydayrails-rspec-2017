5c2a4aa9d590c6d268995e5eaa10f86af1bc702c
mrBuild19 <rabits.tank@gmail.com>
2020-07-24 11:10:25 +0900

    projectモデルのバリデーションテスト2

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,20 +2,48 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
 
-  # ユーザー単位では重複したプロジェクト名を許可しないこと
-  it "does not allow duplicate project names per user" do
-  	user = User.create(
+	before do
+
+		@user = User.create(
   		first_name:  "Joe",
   		last_name:   "Tester",
   		email:       "tester@example.com",
   		password:    "dottle-nouveau-pavilion-tights-furze",
   	)
 
-  	user.projects.create(
+	end
+
+	# プロジェクト名があれば有効な状態であること
+	it "is valid with a project name" do
+
+		project = @user.projects.build(
+			name: "Test Project",
+		)
+
+		expect(project).to be_valid
+
+	end
+
+	# プロジェクト名がなければ無効な状態であること
+	it "is invalid with a project name" do
+
+		project = @user.projects.new(name: nil)
+
+		project.valid?
+
+		expect(project.errors[:name]).to include("can't be blank")
+
+	end
+
+
+  # ユーザー単位では重複したプロジェクト名を許可しないこと
+  it "does not allow duplicate project names per user" do
+
+  	@user.projects.create(
   		name: "Test Project",
   	)
 
-  	new_project = user.projects.build(
+  	new_project = @user.projects.build(
   		name: "Test Project",
   	)
 
@@ -28,14 +56,8 @@ RSpec.describe Project, type: :model do
 
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
-  	user = User.create(
-  		first_name:  "Joe",
-  		last_name:   "Tester",
-  		email:       "tester@example.com",
-  		password:    "dottle-nouveau-pavilion-tights-furze",
-  	)
 
-  	user.projects.create(
+  	@user.projects.create(
   		name: "Test Project",
   	)
 
