29248c1a118ca66f50b9293c288c75966b058eb6
mrBuild19 <rabits.tank@gmail.com>
2020-07-25 11:09:14 +0900

    projectsコントローラのcreate,updateアクションのテストを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -114,6 +114,93 @@ RSpec.describe ProjectsController, type: :controller do
 
 		end
 
+		describe "#create" do
+
+			# 認証済みのユーザーとして
+			context "as an authenticated user" do
+
+				before do
+
+					@user = FactoryBot.create(:user)
+
+				end
+
+				# プロジェクトを追加できること
+				it "adds a project" do
+
+					project_params = FactoryBot.attributes_for(:project)
+
+					sign_in @user
+
+					expect {
+
+						post :create, params: { project: project_params }
+
+					}.to change(@user.projects, :count).by(1)
+
+				end
+
+			end
+
+			# ゲストとして
+			context "as a guest" do
+
+				# 302レスポンスを返すこと
+				it "returns a 302 response" do
+
+					project_params = FactoryBot.attributes_for(:project)
+
+					post :create, params: { project: project_params }
+
+					expect(response).to have_http_status "302"
+
+				end
+
+				# サインイン画面にリダイレクトすること
+				it "redirects to the sign-in page" do
+
+					project_params = FactoryBot.attributes_for(:project)
+
+					post :create, params: { project: project_params }
+
+					expect(response).to redirect_to "/users/sign_in"
+
+				end
+
+			end
+
+		end
+
+		describe "#update" do
+
+			# 認可されたユーザーとして
+			context "as an authorized user" do
+
+				before do
+
+					@user = FactoryBot.create(:user)
+
+					@project = FactoryBot.create(:project, owner: @user)
+
+				end
+
+				# プロジェクトを更新できること
+				it "updates a project" do
+
+					project_params = FactoryBot.attributes_for(:project, name: "New Project Name")
+
+					sign_in @user
+
+					patch :update, params: { id: @project.id, project: project_params }
+
+					expect(@project.reload.name).to eq "New Project Name"
+
+				end
+
+			end
+
+		end
+
 	end
 
 end
